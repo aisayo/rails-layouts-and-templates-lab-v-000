d40c72e1b352b20e34f651763e20fc7c67616f52
aisayo <aisayo@icloud.com>
2017-10-28 18:01:44 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,11 +1,12 @@
 class StoreAdminController < ApplicationController
   layout "admin"
-  layout "order_administration"
 
   def home
   end
 
   def orders
+    render :layout => "order_administration"
   end
+
 
 end
